b76f3b85e7f4d08d14f518dabad4098d04f42201
tomi <tomasbert@hotmail.com>
2015-04-01 09:45:18 -0300

    Modificacion del Main

--- Mortal Kombat/src/model/main.cpp
+++ Mortal Kombat/src/model/main.cpp
@@ -26,70 +26,59 @@ int main( int argc, char* args[] )
 		printf( "Error al inicializar!\n" );
 	} else {
 	
-	//HAGO LO DEL JSON PARA PROBAR
-	Frame* framesCaminar[9];
-	Frame* framesInitial[9];
-	int wCaminar = 68,wInitial = 72;
-	for (int i=0;i<9;i++){
-		framesCaminar[i] = new Frame(wCaminar*i,0,133,wCaminar);
-		framesInitial[i] = new Frame(wInitial*i,0,133,wInitial);
-	}
-	std::string rutaCaminar = "data/players/subzero/sprites/walk.png";
-	std::string rutaInitial = "data/players/subzero/sprites/initial.png";
-	Sprite* Caminar = new Sprite(rutaCaminar,framesCaminar,getRenderer());
-	Sprite* Initial = new Sprite(rutaInitial,framesInitial,getRenderer());
-	Sprite* sprites[2];
-	sprites[0] = Initial;
-	sprites[1] = Caminar;
-	
-	
-		//Load media
+		//HAGO LO DEL JSON PARA PROBAR
+		//Creo los Sprite Manualmente
+		Frame* framesCaminar[9];
+		Frame* framesInitial[9];
+		int wCaminar = 68,wInitial = 72;
+		for (int i=0;i<9;i++){
+			framesCaminar[i] = new Frame(wCaminar*i,0,133,wCaminar);
+			framesInitial[i] = new Frame(wInitial*i,0,133,wInitial);
+		}
+		std::string rutaCaminar = "data/players/subzero/sprites/walk.png";
+		std::string rutaInitial = "data/players/subzero/sprites/initial.png";
+		Sprite* Caminar = new Sprite(rutaCaminar,framesCaminar,getRenderer());
+		Sprite* Initial = new Sprite(rutaInitial,framesInitial,getRenderer());
+		Sprite* sprites[2];
+		sprites[0] = Initial;
+		sprites[1] = Caminar;
+		//Fin creo Sprite manualmente
 
-		//if( !loadMedia( luchador.Sprite(0) ) ){
-		//	printf( "Error al cargar media!\n" );
-		//} else {
+		//Creo el Personaje
+		Personaje luchador = Personaje("Sub Zero",sprites);
 
-			//Creo el Personaje
-			Personaje luchador = Personaje("Sub Zero",sprites);
+		//Creo el Controlador
+		KeyboardControl control = KeyboardControl(&luchador);
 
-			//Creo el Controlador
-			KeyboardControl control = KeyboardControl(&luchador);
+		//flag para el Loop Principal
+		bool quit = false;
 
-			//flag para el Loop Principal
-			bool quit = false;
+		//Manejador de evento
+		SDL_Event e;
 
-			//Manejador de evento
-			SDL_Event e;
+		//Current animation frame
+		int frame = 0;
 
-			//Current animation frame
-			int frame = 0;
-
-			//While Principal
-			while( !quit ){
-				//Eventos
-				while( SDL_PollEvent( &e ) != 0 ){
-					//Usuario presiona [X]
-					if( e.type == SDL_QUIT ){
-						quit = true;
-					}
-					control.KeyPressed(e);
+		//While Principal
+		while( !quit ){
+			//Eventos
+			while( SDL_PollEvent( &e ) != 0 ){
+				//Usuario presiona [X]
+				if( e.type == SDL_QUIT ){
+					quit = true;
 				}
+				control.KeyPressed(e);
+			}
 
-				printf("%i\n",frame);
-				Refresh(luchador.getSpriteActual(),SCREEN_WIDTH,SCREEN_HEIGHT);
+			printf("%i\n",frame);
 
-				//Ir al siguiente estado
-				++frame;
+			Refresh(luchador.getSpriteActual(),SCREEN_WIDTH,SCREEN_HEIGHT);
 
-				//Ciclo de animacion
-				//if( frame >= 9 ){
-				//	frame = 0;
-				//}
+			//FrameGuia
+			++frame;
 
-				//Sleep(Microsegundos)
-				usleep(100000);
-
-			//}
+			//Sleep(Microsegundos)
+			usleep(100000);
 		}
 	}
 
