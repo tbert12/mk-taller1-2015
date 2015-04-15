5e174a4f295937f3ffe375ed5d1d9b7a425cf834
facu <facu@Facu-Unix>
2015-04-14 21:44:29 -0300

    perfectoo, mirenlo

--- Mortal Kombat/src/model/main.cpp
+++ Mortal Kombat/src/model/main.cpp
@@ -22,7 +22,7 @@
 #include "../controller/KeyboardControl.h"
 #include "Personaje.h"
 
-string ruta_archivo_configuracion = "data/config/default.json";
+string ruta_archivo_configuracion = "data/config/TheSubway.json";
 
 int main( int argc, char* args[] )
 {
