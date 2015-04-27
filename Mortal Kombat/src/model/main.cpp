7e5c6d81e3ed8b004d9c2ee014b138ac50f21357
tomi <tomasbert@hotmail.com>
2015-04-27 14:19:13 -0300

    Algunas cosas acomodadas en el main

--- Mortal Kombat/src/model/main.cpp
+++ Mortal Kombat/src/model/main.cpp
@@ -25,6 +25,29 @@
 
 string ruta_archivo_configuracion = "data/config/Parallax.json";
 
+Mundo* cargarMundo(){
+	Mundo* unMundo;
+	ParserJSON* parser;
+	try {
+			parser = new ParserJSON( ruta_archivo_configuracion );
+			unMundo = parser->cargarMundo();
+			log( "Se creo correctamente el Mundo de la partida.", LOG_DEBUG );
+			delete parser;
+		} catch ( std::exception &e ) {
+			log( "No se pudo crear el Mundo. Se aborta la ejecucion del programa. " + string(e.what()), LOG_ERROR );
+			delete parser;
+			delete unMundo;
+			return NULL;
+	}
+	return unMundo;
+}
+
+void free(Mundo* mundo,KeyboardControl1* c1,KeyboardControl2* c2){
+	delete mundo;
+	delete c1;
+	delete c2;
+}
+
 int main( int argc, char* args[] )
 {
 
@@ -36,24 +59,8 @@ int main( int argc, char* args[] )
 		ruta_archivo_configuracion = args[1];
 	}
 
-
-	ParserJSON* parser;
-	Mundo* mundo;
-
-	try {
-		parser = new ParserJSON( ruta_archivo_configuracion );
-		mundo = parser->cargarMundo();
-		log( "Se creo correctamente el Mundo de la partida.", LOG_DEBUG );
-	} catch ( std::exception &e ) {
-		log( "No se pudo crear el Mundo. Se aborta la ejecucion del programa. " + string(e.what()), LOG_ERROR );
-		delete parser;
-		delete mundo;
-		return 1;
-	}
-
-	//Creo los Personajes
-	Personaje* luchador1 = mundo->getPersonaje(0);
-	Personaje* luchador2 = mundo->getPersonaje(1);
+	Mundo* mundo = cargarMundo();
+	if (mundo == NULL) return 1;
 
 
 	//mostrar imagen inicio
@@ -63,57 +70,44 @@ int main( int argc, char* args[] )
 	}
 
 	//Creo los Controladores
-	KeyboardControl1* control_jugador_1 = new KeyboardControl1(luchador1);
-	KeyboardControl2* control_jugador_2 = new KeyboardControl2(luchador2);
+	KeyboardControl1* control_jugador_1 = new KeyboardControl1(mundo->getPersonaje(0));
+	KeyboardControl2* control_jugador_2 = new KeyboardControl2(mundo->getPersonaje(1));
 
 	//While Principal
 	while( !control_jugador_1->getQuit() && !control_jugador_2->getQuit() ){
 		control_jugador_1->KeyState();
+		control_jugador_2->KeyState();
 		//Eventos */
-		while( control_jugador_1->PollEvent() ){
+		while( control_jugador_1->PollEvent() or control_jugador_2->PollEvent() ){
 			try {
+
 				control_jugador_1->KeyPressed();
-			// RELOAD
-			// ESTA HECHO A LO VILLA PORQUE TOTAL DESPUES ESTO VUELA A LA MIERDA.
+				control_jugador_2->KeyPressed();
+
 			} catch ( std::runtime_error &e ) {
 				log ( "Refresh. Se recarga el mundo a partir del mismo archivo de configuracion JSON.", LOG_DEBUG );
-				delete parser;
-				delete control_jugador_1;
-				delete control_jugador_2;
-				delete mundo;
+				free(mundo,control_jugador_1,control_jugador_2);
 
-				try {
-					parser = new ParserJSON( ruta_archivo_configuracion );
-					mundo = parser->cargarMundo();
-					log( "Se creo correctamente el Mundo de la partida.", LOG_DEBUG );
-					//Creo los Personajes
-					luchador1 = mundo->getPersonaje(0);
-					luchador2 = mundo->getPersonaje(1);
-					//Creo los Controladores
-					control_jugador_1 = new KeyboardControl1(luchador1);
-					control_jugador_2 = new KeyboardControl2(luchador2);
-				} catch ( std::runtime_error &e ) {
-					log( "No se pudo crear el Mundo. Se aborta la ejecucion del programa. " + string(e.what()), LOG_ERROR );
-					return 1;
+				mundo = cargarMundo();
+				if (mundo == NULL) return 1;
+				log( "Se creo correctamente el Mundo de la partida.", LOG_DEBUG );
 
-				}
+				//Creo los Controladores
+				control_jugador_1 = new KeyboardControl1(mundo->getPersonaje(0));
+				control_jugador_2 = new KeyboardControl2(mundo->getPersonaje(1));
 
 			}
 		}
-		control_jugador_2->KeyState();
-		while ( control_jugador_2->PollEvent() ) {
-			control_jugador_2->KeyPressed();
-		}
 
 		mundo->render();
+
+
 		//Sleep(Microsegundos)
 		usleep(50000);
-
 	}
-	delete parser;
-	delete control_jugador_1;
-	delete control_jugador_2;
-	delete mundo;
+
+
+	free(mundo,control_jugador_1,control_jugador_2);
 	log("Se cierra el programa y se libera la memoria correspondiente al Mundo",LOG_DEBUG);
 
 	return 0;
