f573e700652229412c501ed30a6063fa3eee84c2
facu <facu@Facu-Unix>
2015-04-29 13:11:25 -0300

    algo de joys

--- Mortal Kombat/src/controller/JoystickControl.cpp
+++ Mortal Kombat/src/controller/JoystickControl.cpp
@@ -7,18 +7,63 @@
 
 #include "JoystickControl.h"
 
-JoystickControl::JoystickControl() {
+JoystickControl::JoystickControl(SDL_Event e,Personaje* un_personaje) {
+	personaje = un_personaje;
+	evento = NULL;
+	_Init();
+}
+
+void JoystickControl::_Init(){
 	joystick =	NULL;
-	cant_joystick = SDL_NumJoysticks();
-	if(cant_joystick < 1) log("No hay ningun joystick conectado",LOG_WARNING);
-	joystick = SDL_JoystickOpen( 0 );
+	if(SDL_NumJoysticks() < 1) log("No hay ningun joystick conectado",LOG_WARNING);
+	else joystick = SDL_JoystickOpen( 0 );
 	if( joystick == NULL ) log(string("No se puede leer el joystick! SDL Error: %s\n", SDL_GetError()),LOG_ERROR);
 
+}
+
+bool JoystickControl::PollEvent(){
+	return (SDL_PollEvent( &evento ) != 0);
+}
+
+void JoystickControl::JoyPressed(){
+	//movimiento del ANALOGICO
+	if( evento.type == SDL_JOYAXISMOTION ){
+		//En el analogico de la izquierda
+		if( evento.jaxis.which == 0 ){
+			//X axis motion
+			if( evento.jaxis.axis == 0 ){
+				//Left of dead zone
+				if( evento.jaxis.value < -JOYSTICK_DEAD_ZONE ){
+					//IZQUIERDA
+				}
+				//Right of dead zone
+				else if( evento.jaxis.value > JOYSTICK_DEAD_ZONE ){
+					//DERECHA
+				}
+				else{
+					//NO SE
+				}
+			}
+			//Y axis motion
+			else if( evento.jaxis.axis == 1 ){
+				//Below of dead zone
+				if( evento.jaxis.value < -JOYSTICK_DEAD_ZONE ){
+					//ABAJO
+				}
+				//Above of dead zone
+				else if( evento.jaxis.value > JOYSTICK_DEAD_ZONE ){
+					//ARRIBA
+				}
+				else{
+					//NADA
+				}
+			}
+		}
+	}
 }
 
 JoystickControl::~JoystickControl() {
 	SDL_JoystickClose( joystick );
 	joystick = NULL;
-	cant_joystick = 0;
 }
 
--- Mortal Kombat/src/controller/JoystickControl.h
+++ Mortal Kombat/src/controller/JoystickControl.h
@@ -8,18 +8,31 @@
 #ifndef SRC_CONTROLLER_JOYSTICKCONTROL_H_
 #define SRC_CONTROLLER_JOYSTICKCONTROL_H_
 
+//maximo en el analogico
+#define JOYSTICK_DEAD_ZONE 8000
+
 #include "../model/logging.h"
+#include "../model/Personaje.h"
 #include <SDL2/SDL.h>
 
+//CUANTOS JOYSTICK, para el parser seguro!
+int cantidad_de_Joystick(){
+	return SDL_NumJoysticks();
+}
 
 class JoystickControl {
 public:
-	JoystickControl();
+	JoystickControl(SDL_Event e,Personaje* un_personaje);
+	bool PollEvent();
+	void JoyPressed();
 	virtual ~JoystickControl();
 
 private:
+	SDL_Event evento;
 	SDL_Joystick* joystick;
-	int cant_joystick;
+	Personaje* personaje;
+
+	void _Init();
 };
 
 #endif /* SRC_CONTROLLER_JOYSTICKCONTROL_H_ */
