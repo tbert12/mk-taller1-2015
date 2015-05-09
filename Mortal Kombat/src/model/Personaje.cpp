6d73d22020e17494275da25880e0328f02f459dd
tomi <tomasbert@hotmail.com>
2015-05-08 23:17:11 -0300

    Patada y Pina en salto

--- Mortal Kombat/src/model/Personaje.cpp
+++ Mortal Kombat/src/model/Personaje.cpp
@@ -165,6 +165,17 @@ float Personaje::getAlto(){
 	return spriteActual->getAlto();
 }
 
+int Personaje::getAccionDeAtaque(){
+	//Devuelve -1 si no encuentra la Accion del Sprite
+	//Nunca va a pasar, pero por el compilador
+	for (int i=0; i < sprites.size(); i++){
+		if (sprites[i] == spriteActual){
+			return i;
+		}
+	}
+	return -1;
+}
+
 int Personaje::getSentidoDeMovimiento(){
 	if (m_velocidadActual > 0) return 1;
 	else if (m_velocidadActual < 0) return -1;
@@ -256,6 +267,7 @@ void Personaje::AvanzarSprite(){
 		if (_estaAtacando) _estaAtacando = false;
 	}
 	if ( _estaSaltando > 0){
+		// Alguien vio alguna vez un if tan feo como este
 		//Salteo en el Aire el impulso del salto diagonal
 		if (m_velocidadActual > 0){
 			if (!m_fliped){
@@ -282,16 +294,23 @@ void Personaje::AvanzarSprite(){
 
 void Personaje::_cambiarSprite(int SpriteAccion){
 	if(_estaSaltando > 0) {
-		if (SpriteAccion != SPRITE_PINA_SALTANDO)
-			printf("Salis\n");
+		//Ataque en saltos
+		if (SpriteAccion == SPRITE_PINA_SALTANDO or SpriteAccion == SPRITE_PATADA_SALTANDO){
+			//SOY BURRO, NOSE COMO HACER ESTE IF MAS LINDO
+		} else {
 			return;
+		}
 	}
+	//Esto es feo Tomi
 	if (spriteActual == sprites[SPRITE_MUERE]) return;
+
+	//Excepcion del caminar para cambiarlo repentinamente
 	if (sprites[SpriteAccion] == spriteActual and SpriteAccion != SPRITE_CAMINAR) return;
 
-	if (SpriteAccion == SPRITE_PINA_SALTANDO) printf("ESTOY ACA2\n");
-
+	//Dejo el Sprite Limpio antes de cambiarlo
+	if (SpriteAccion != SPRITE_CAMINAR) spriteActual->hardReset();
 	spriteActual = sprites[SpriteAccion];
+
 	if (SpriteAccion == SPRITE_CAMINAR or
 		SpriteAccion == SPRITE_SALTAR_DIAGONAL){
 		bool doReverse;
@@ -500,7 +519,10 @@ void Personaje::_gancho() {
 }
 
 void Personaje::_pinaSaltando() {
-	_cambiarSprite(SPRITE_PINA_SALTANDO);
+	if ( tiempoDeSalto >= TIEMPOTOTALDESALTO/3 ){
+		_cambiarSprite(SPRITE_PINA_SALTANDO);
+		spriteActual->doLoop(true);
+	}
 }
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -543,9 +565,9 @@ void Personaje::_patadaCircular() {
 }
 
 void Personaje::_patadaSaltando() {
-	if ( tiempoDeSalto >= TIEMPOTOTALDESALTO/2 ){
+	if ( tiempoDeSalto >= TIEMPOTOTALDESALTO/4 ){
 		_cambiarSprite(SPRITE_PATADA_SALTANDO);
-		spriteActual->setLoop(true);
+		spriteActual->doLoop(true);
 	}
 }
 
@@ -561,25 +583,25 @@ void Personaje::_patadaAltaAgachado() {
 void Personaje::recibirGolpe(int CodigoGolpe, int Danio){
 	//El Codigo de golpe esta seteados en todos el mismo
 	/*
-		--->SPRITE_RECIBE_GANCHO<---
+		--->SPRITE_RECIBE_GANCHO<--- (-10 de Vida)
 		GANCHO=         	   12;
 
-		--->SPRITE_RECIBE_FUERTE<--
+		--->SPRITE_RECIBE_FUERTE<--- (-10 de Vida)
 
-		--->SPRITE_RECIBE_ALTO
+		--->SPRITE_RECIBE_ALTO<----- (-8 de Vida)
 		PINA_ALTA=		 	   19;
 		PATADA_ALTA=		   14;
 		PATADA_SALTANDO=	   17;
 		PINA_SALTANDO=  	   21;
 
-		--->SPRITE_RECIBE_BAJO || SPRITE_RECIBE_AGACHADO<---
+		--->SPRITE_RECIBE_BAJO || SPRITE_RECIBE_AGACHADO<--- (-5 de Vida)
 		PINA_AGACHADO=  	   18;
 		PATADA_ALTA_AGACHADO=  5;
 		PINA_BAJA=		 	   20;
 		PATADA_BAJA_AGACHADO=  6;
 		PATADA_BAJA=    	   15;
 
-		--->SPRITE_RECIBE_PATADA_GIRA<--
+		--->SPRITE_RECIBE_PATADA_GIRA<-- (-8 de Vida)
 		PATADA_CIRCULAR=       16;
 
 	*/
@@ -600,7 +622,7 @@ void Personaje::recibirGolpe(int CodigoGolpe, int Danio){
 }
 
 //-------------------------------------------------------------------------------------------------------------------------
-//FIN MANEJO DE SPRITES
+//FIN MANEJO DE SPRITES :)
 
 Personaje::~Personaje() {
 	spriteActual = NULL;
