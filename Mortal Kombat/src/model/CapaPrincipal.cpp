507fa1a9073adaf07423778affb6d1ed0f41b294
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2015-05-10 20:24:43 -0300

    Colisiones

--- Mortal Kombat/src/model/CapaPrincipal.cpp
+++ Mortal Kombat/src/model/CapaPrincipal.cpp
@@ -104,6 +104,8 @@ void CapaPrincipal::_CheckearColisiones(Personaje* personaje, Personaje* persona
 	Rect_Logico* rectAtaque2 = personajeFlippeado->rectanguloAtaque();
 	Rect_Logico* rectDefensa2 = personajeFlippeado->rectanguloDefensa();
 
+	ObjetoArrojable* objetoSinFlip = personaje->getPoderActivo();
+	ObjetoArrojable* objetoConFlip = personajeFlippeado->getPoderActivo();
 	//printf("actual: %f, anterior: %f, defensa: %f\n",rectAtaque1->x + rectAtaque1->w ,rectAtaqueAnterior1->x + rectAtaqueAnterior1->w,rectDefensa2->x );
 
 	if (rectAtaque1 != NULL ){
@@ -113,42 +115,49 @@ void CapaPrincipal::_CheckearColisiones(Personaje* personaje, Personaje* persona
 			rectAtaqueAnterior1 = rectDefensa1;
 		}
 
-		bool colisionaAtaquePersonajeSinFlipX = (floatIsBetween(rectDefensa2->x - rectDefensa2->w,rectAtaque1->x,rectAtaque1->w) and
-				!floatIsBetween(rectDefensa2->x - rectDefensa2->w,rectAtaqueAnterior1->x,rectAtaqueAnterior1->w));
+		bool colisionaAtaquePersonajeSinFlipX = (floatIsBetween(rectDefensa2->x ,rectAtaque1->x,rectAtaque1->w) and
+				!floatIsBetween(rectDefensa2->x,rectAtaqueAnterior1->x,rectAtaqueAnterior1->w));
 
 		bool colisionaY = (floatIsBetween(rectDefensa2->y, rectAtaque1->y-rectAtaque1->h , rectAtaque1->h) and floatIsBetween(rectDefensa2->y, rectAtaqueAnterior1->y-rectAtaqueAnterior1->h, rectAtaqueAnterior1->h)) or
 				(floatIsBetween(rectAtaque1->y, rectDefensa2->y-rectDefensa2->h , rectDefensa2->h) and floatIsBetween(rectAtaqueAnterior1->y, rectDefensa2->y-rectDefensa2->h, rectDefensa2->h));
 
 		if( colisionaAtaquePersonajeSinFlipX and colisionaY){
-			printf("hubo colision\n");
+			printf("hubo colision ppsf\n");
 		}
-		/*
-		else
-		{
-			printf("rectAtaqueEnd: %f \n",rectAtaque1->x + rectAtaque1->w);
-			printf("rectAtaqueAnteriorEnd: %f \n",rectAtaqueAnterior1->x );
-			printf("rectDefensaStart: %f \n",rectDefensa2->x);
-		}
-		*/
-			//if(rectAtaque1->x + rectAtaque1->w >= rectDefensa2->x and rectAtaqueAnterior1->x + rectAtaqueAnterior1->w < rectDefensa2->x  )
-			//	printf("hubo colision Personaje1 Ataco y personaje 2 recibio\n");
 	}else if (rectAtaque2 != NULL ){
 		if(!rectAtaqueAnterior2){
 			printf("uso rect defensa \n");
 			rectAtaqueAnterior2 = rectDefensa2;
 		}
+		bool colisionAtaquePersonajeConFlipX = (floatIsBetween(rectDefensa1->x  + rectDefensa1->w, rectAtaque2->x , rectAtaque2->w) and
+						!floatIsBetween(rectDefensa1->x + rectDefensa1->w, rectAtaqueAnterior2->x, rectAtaqueAnterior2->w));
 
-		bool colisionAtaquePersonajeConFlipX = (floatIsBetween(rectDefensa1->x , rectAtaque2->x - rectAtaque2->w, rectAtaque2->w) and
-				!floatIsBetween(rectDefensa1->x, rectAtaqueAnterior2->x - rectAtaqueAnterior2->w, rectAtaqueAnterior2->w));
-		printf("colisionAtaquePersonajeConFlip: %i \n",colisionAtaquePersonajeConFlipX);
+		//printf("colisionAtaquePersonajeConFlip: %i \n",colisionAtaquePersonajeConFlipX);
 		bool colisionaY = (floatIsBetween(rectDefensa1->y, rectAtaque2->y-rectAtaque2->h , rectAtaque2->h) and floatIsBetween(rectDefensa1->y, rectAtaqueAnterior2->y-rectAtaqueAnterior2->h, rectAtaqueAnterior2->h)) or
 				(floatIsBetween(rectAtaque2->y, rectDefensa1->y-rectDefensa1->h , rectDefensa1->h) and floatIsBetween(rectAtaqueAnterior2->y, rectDefensa1->y-rectDefensa1->h, rectDefensa1->h));
 		//and colisionaY
 		if( colisionAtaquePersonajeConFlipX  and colisionaY){
-			//printf("rectAtaqueStart: %f \n",rectAtaque2->x - rectAtaque2->w);
-			//printf("rectAtaqueAnteriorStart: %f \n",rectAtaqueAnterior2->x - rectAtaqueAnterior2->w);
-			//printf("rectDefensaEnd: %f \n",rectDefensa1->x + rectDefensa1->w);
-			printf("hubo colision\n");
+			printf("hubo colision  ppcf\n");
+		}
+	}
+
+	if (objetoSinFlip != NULL ){
+
+		Rect_Logico* rectPoder = objetoSinFlip->rectanguloAtaque();
+		if(rectPoder->x>rectDefensa2->x)
+			return;
+		bool colisionaAtaquePersonajeSinFlipX = (floatIsBetween(rectDefensa2->x ,rectPoder->x,rectPoder->w) and
+				!floatIsBetween(rectDefensa2->x,rectPoder->x,rectPoder->w));
+
+		printf("RectAtaqueX :%f\n",rectPoder->x);
+		printf("RectAtaquew :%f\n",rectPoder->w);
+		printf("RectdefensaPos :%f\n",rectDefensa2->x);
+
+		bool colisionaY = (floatIsBetween(rectDefensa2->y, rectPoder->y-rectPoder->h , rectPoder->h) and floatIsBetween(rectDefensa2->y, rectPoder->y-rectPoder->h, rectPoder->h)) or
+				(floatIsBetween(rectPoder->y, rectDefensa2->y-rectDefensa2->h , rectDefensa2->h) and floatIsBetween(rectPoder->y, rectDefensa2->y-rectDefensa2->h, rectDefensa2->h));
+
+		if( colisionaAtaquePersonajeSinFlipX and colisionaY){
+			printf("hubo colision ppsf\n");
 		}
 	}
 
