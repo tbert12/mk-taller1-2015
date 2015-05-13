579ef6fb46e4ff6805ae1b387c66c59cfd4f850f
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2015-05-13 17:26:12 -0300

    Colision de objeto flipeado agachado

--- Mortal Kombat/src/model/CapaPrincipal.cpp
+++ Mortal Kombat/src/model/CapaPrincipal.cpp
@@ -273,7 +273,7 @@ int CapaPrincipal::_CheckearColisiones(Personaje* personaje, Personaje* personaj
 				and	not(rectDefensa1->x > rectPoder->x + rectPoder->w );
 
 		bool colisionaY = (floatIsBetween(rectDefensa1->y, rectPoder->y-rectPoder->h , rectPoder->h) and floatIsBetween(rectDefensa1->y, rectPoder->y-rectPoder->h, rectPoder->h)) or
-				(floatIsBetween(rectPoder->y, rectDefensa1->y-rectDefensa1->h , rectDefensa2->h) and floatIsBetween(rectPoder->y, rectDefensa1->y-rectDefensa1->h, rectDefensa1->h));
+				(floatIsBetween(rectPoder->y, rectDefensa1->y-rectDefensa1->h , rectDefensa1->h) and floatIsBetween(rectPoder->y, rectDefensa1->y-rectDefensa1->h, rectDefensa1->h));
 
 		if( colisionaAtaquePersonajeConFlipX and colisionaY){
 			return COLISION_PERSONAJE_OBJETO_CON_FLIP;
