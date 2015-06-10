631d05041e605122250f4b1f50991df4040c32e9
tomi <tomasbert@hotmail.com>
2015-06-10 16:33:38 -0300

    Json!

---
+++ Mortal Kombat/src/model/personajes/Jax.cpp
@@ -0,0 +1,21 @@
+/*
+ * Jax.cpp
+ *
+ *  Created on: 10/6/2015
+ *      Author: tomi
+ */
+
+#include "Jax.h"
+
+namespace std {
+
+Jax::Jax() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Jax::~Jax() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace std */
