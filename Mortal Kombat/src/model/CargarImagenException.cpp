6baf395bf8bb8cdf40d8e38b1872c6987908f6ab
tbert <tomasbert12@gmail.com>
2017-10-08 20:59:48 -0300

    Remove Mortal Kombar folder

--- Mortal Kombat/src/model/CargarImagenException.cpp
+++
@@ -1,9 +0,0 @@
-#include "CargarImagenException.h"
-
-CargarImagenException::CargarImagenException( string ruta_imagen )
-	: runtime_error( "Error al intentar cargar la imagen de la ruta especificada." ), ruta_imagen( ruta_imagen )
-	{}
-
-const char* CargarImagenException::what() const throw() {
-	return ( "Error al intentar cargar la imagen de la ruta especificada: " + ruta_imagen ).c_str();
-}
